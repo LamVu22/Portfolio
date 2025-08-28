000569512d9f55d74cbe1e471bce29a80bb41f9a
Vu An Bao Lam <lamvu@Mac.lan>
2025-08-28 16:47:57 -0400

    Initial commit

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,97 @@
+import { Briefcase, Code, User } from "lucide-react";
+
+export const AboutSection = () => {
+  return (
+    <section id="about" className="py-24 px-4 relative">
+      {" "}
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+          About <span className="text-primary"> Me</span>
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold">
+              Data-driven Developer & Full Stack Problem Solver
+            </h3>
+
+            <p className="text-muted-foreground">
+              I'm a Computer Science student specializing 
+              in Computational Data Science, with passion for AI and ML and hands-on experience 
+              building full-stack applications, real-time detection systems, 
+              and dynamic dashboards that drive operational insights.
+            </p>
+
+            <p className="text-muted-foreground">
+              From designing scalable APIs and data visualizations to deploying 
+              client-facing tools, I’m passionate about turning complex data 
+              and system requirements into efficient, user-centered solutions—always 
+              with a focus on impact and clarity.
+            </p>
+
+            <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+              <a href="#contact" className="cosmic-button">
+                {" "}
+                Get In Touch
+              </a>
+
+              <a
+                href="./CV/Vu_Resume.pdf"
+                download="Vu_Resume.pdf"
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+              >
+                Download CV
+              </a>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 gap-6">
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Code className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Full-Stack Development </h4>
+                  <p className="text-muted-foreground">
+                    Building end-to-end web apps with efficient APIs, 
+                    secure authentication, and responsive UIs.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <User className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Data Analytics & Dashboards</h4>
+                  <p className="text-muted-foreground">
+                    Delivering clear insights through KPI dashboards, 
+                    SQL pipelines, and real-time tracking systems.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Briefcase className="h-6 w-6 text-primary" />
+                </div>
+
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Project Management</h4>
+                  <p className="text-muted-foreground">
+                    Leading projects from conception to completion with agile
+                    methodologies.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
